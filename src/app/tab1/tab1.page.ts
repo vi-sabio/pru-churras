783b8401fe52f7366a0432a2ab9f6083eef9e188
Aline Otília <aos_santos@hotmail.com>
2023-02-10 20:37:18 -0300

    Cara ou Coroa

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -3,10 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
+  moeda = 'assets/logo.png';
+  cara = 'assets/cara.png';
+  coroa = 'assets/coroa.png';
+  info = 'Clique em jogar a moeda!';
+  botaoInfo = 'Jogar a moeda!';
 
   constructor() {}
 
+  jogarMoeda() {
+    if (Math.random() < 0.5) {
+      this.moeda = this.cara;
+      this.info = 'Cara!';
+    } else {
+      this.moeda = this.coroa;
+      this.info = 'Coroa!';
+    }
+
+    this.botaoInfo = 'Jogar Novamente';
+  }
 }
